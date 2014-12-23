a48f8d9cf223a304db925cb52052cb26db1a4619
Ali-Alzyoud <alialzyoud@alis-MacBook-Pro.local>
2014-12-23 12:29:25 +0200

    Initial Commit

---
+++ Zori/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Zori
+//
+//  Created by ali alzyoud on 12/23/14.
+//  Copyright (c) 2014 ProjectX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Zori/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Zori
+//
+//  Created by ali alzyoud on 12/23/14.
+//  Copyright (c) 2014 ProjectX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
